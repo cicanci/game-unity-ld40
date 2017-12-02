f44dfe6564918642774ada852e7f4e16a0ae9e74
Bruno Cicanci <brunocicanci@gmail.com>
2017-12-02 12:27:50 -0200

    Generate block ground with offset

--- Assets/Scripts/GroundGenerator.cs
+++ Assets/Scripts/GroundGenerator.cs
@@ -5,12 +5,35 @@ public class GroundGenerator : MonoBehaviour
     [SerializeField]
     private GameObject _groundBlockPrefab;
 
-    [SerializeField] 
+    [SerializeField]
     private Vector2 _boardSize;
-    
+
+    [SerializeField]
+    private Vector3 _initialPosition;
+
+    [SerializeField]
+    private float _spaceOffset;
+
     private void Start()
     {
-        GameObject block = Instantiate(_groundBlockPrefab);
-        
+        GenerateGround();
+    }
+
+    private void GenerateGround()
+    {
+        for(int i = 0; i < _boardSize.x; i++)
+        {
+            for(int j = 0; j < _boardSize.y; j++)
+            {
+                GameObject block = Instantiate(_groundBlockPrefab);
+                block.transform.parent = transform;
+
+                Vector3 position = block.transform.localPosition - _initialPosition;
+                block.transform.localPosition = new Vector3(
+                    position.x + (i * _spaceOffset),
+                    position.y,
+                    position.z + (j * _spaceOffset));
+            }
+        }
     }
 }
