87fbf1fb5bdfd5a65a1608cb5de018eeb2a363d9
Bruno Cicanci <brunocicanci@gmail.com>
2017-12-02 12:07:00 -0200

    Script for ground generation

---
+++ Assets/Scripts/GroundGenerator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GroundGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _groundBlockPrefab;
+
+    [SerializeField] 
+    private Vector2 _boardSize;
+    
+    private void Start()
+    {
+        GameObject block = Instantiate(_groundBlockPrefab);
+        
+    }
+}
