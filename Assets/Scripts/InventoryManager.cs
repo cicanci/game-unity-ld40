2fd32a44cf73ffd7465410022c2a3221cedeb1e4
Bruno Cicanci <brunocicanci@gmail.com>
2017-12-02 13:51:56 -0200

    Added script to get the remote settings for initial water and food

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+    [Header("Default Values")]
+    [SerializeField]
+    private int _defaultWaterAmount;
+    [SerializeField]
+    private int _defaultFoodAmount;
+
+    [Header("Label Objects")]
+    [SerializeField]
+    private Text _waterAmount;
+    [SerializeField]
+    private Text _foodAmount;
+
+    private void Start()
+    {
+        RemoteSettings.Updated += HandleRemoteUpdate;
+    }
+
+    private void OnDestroy()
+    {
+        RemoteSettings.Updated -= HandleRemoteUpdate;
+    }
+
+    private void HandleRemoteUpdate()
+    {
+        _waterAmount.text = RemoteSettings.GetInt("initial_water", _defaultWaterAmount).ToString();
+        _foodAmount.text = RemoteSettings.GetInt("initial_food", _defaultFoodAmount).ToString();
+    }
+}
