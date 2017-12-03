ac42316294c75e18bc390f8329426f5b8abf163c
Bruno Cicanci <brunocicanci@gmail.com>
2017-12-03 10:17:23 -0200

    Local cache for remote settings

---
+++ Assets/Scripts/RemoteSettingsCache.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class RemoteSettingsCache : MonoBehaviour 
+{
+    [Header("Remote Settings - Local Values")]
+    [SerializeField]
+    private int _defaultInitialWater;
+    [SerializeField]
+    private int _defaultInitialFood;
+    [SerializeField]
+    private int _defaultInitialGold;
+    [SerializeField]
+    private float _defaultWaterPerSecond;
+    [SerializeField]
+    private float _defaultFoodPerSecond;
+    [SerializeField]
+    private float _defaultGoldPerSecond;
+    [SerializeField]
+    private int _defaultMaxWater;
+    [SerializeField]
+    private int _defaultMaxFood;
+    [SerializeField]
+    private int _defaultMaxGold;
+
+    public static int InitialWater { private set; get; }
+    public static int InitialFood { private set; get; }
+    public static int InitialGold { private set; get; }
+    public static float WaterPerSecond { private set; get; }
+    public static float FoodPerSecond { private set; get; }
+    public static float GoldPerSecond { private set; get; }
+    public static int MaxWater { private set; get; }
+    public static int MaxFood { private set; get; }
+    public static int MaxGold { private set; get; }
+
+    public static bool Initialized { private set; get; }
+
+    private void Start()
+    {
+        if(Initialized)
+        {
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+        RemoteSettings.Updated += HandleRemoteUpdate;
+    }
+
+    private void HandleRemoteUpdate()
+    {
+        InitialWater = RemoteSettings.GetInt("initial_water", _defaultInitialWater);
+        InitialFood = RemoteSettings.GetInt("initial_food", _defaultInitialFood);
+        InitialGold = RemoteSettings.GetInt("initial_gold", _defaultInitialGold);
+
+        WaterPerSecond = RemoteSettings.GetFloat("water_per_second", _defaultWaterPerSecond);
+        FoodPerSecond = RemoteSettings.GetFloat("food_per_second", _defaultFoodPerSecond);
+        GoldPerSecond = RemoteSettings.GetFloat("gold_per_second", _defaultGoldPerSecond);
+
+        MaxWater = RemoteSettings.GetInt("max_water", _defaultMaxWater);
+        MaxFood = RemoteSettings.GetInt("max_food", _defaultMaxFood);
+        MaxGold = RemoteSettings.GetInt("max_gold", _defaultMaxGold);
+
+        Initialized = true;
+        RemoteSettings.Updated -= HandleRemoteUpdate;
+    }
+}
