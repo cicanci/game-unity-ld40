23968934b23b761a987614b272b61ca9668b4f67
Bruno Cicanci <brunocicanci@gmail.com>
2017-12-03 20:39:26 -0200

    Ground block army

--- Assets/Scripts/GroundBlock.cs
+++ Assets/Scripts/GroundBlock.cs
@@ -2,5 +2,24 @@
 
 public class GroundBlock : MonoBehaviour 
 {
+    [SerializeField] 
+    private Material _selectedColor;
+
+    [SerializeField] 
+    private GameObject _playerArmyPrefab;
+
+    [SerializeField] 
+    private GameObject _enemyArmyPrefab;
     
+    public int BlockId { set; get; }
+
+    public void SelectBlock()
+    {
+        WorkerManager.Instance.ShowDefensePanel();
+        GetComponent<MeshRenderer>().material = _selectedColor;
+        
+        GameObject block = Instantiate(_playerArmyPrefab);
+        block.transform.parent = transform;
+        block.transform.localPosition = new Vector3(0, 2, 0);
+    }
 }
